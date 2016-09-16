444a64816a7071fc135e22a2313e49fedddb42ae
harsh <hmdesai89@gmail.com>
2016-09-16 15:45:12 +0530

    Adding a describe_instacnes

--- functest/common/sanity_test.py
+++ functest/common/sanity_test.py
@@ -74,6 +74,7 @@ class SanityTest(unittest.TestCase):
 
 
     def test_delete_subnet(self):
+        self.jclient.compute.describe_instances()
         if self.__class__.subnetId :
             time.sleep(5)
             resp = self.jclient.vpc.delete_subnet(subnet_id=self.subnetId)
